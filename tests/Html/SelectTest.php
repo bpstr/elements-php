259ae529d54388e647892e9587dbb45d21ac28ff
Fent János <janos.fent@cheppers.com>
2019-10-21 00:55:51 +0200

    Added more select tests

--- tests/Html/SelectTest.php
+++ tests/Html/SelectTest.php
@@ -26,4 +26,38 @@ final class SelectTest extends TestCase {
 		);
 	}
 
+	public function testDisabledOption(): void {
+		$cities = ['BUD' => 'Budapest', 'KIN' => 'Kingston'];
+		$select = Select::create( 'city', $cities);
+		$select->activate('KIN');
+		$select->addOption('XYZ', NULL, FALSE, TRUE);
+		$this->assertSame(
+			'<select name="city"><option value="BUD">Budapest</option><option value="KIN" selected="selected">Kingston</option><option value="XYZ" disabled="disabled">XYZ</option></select>',
+			(string) $select
+		);
+	}
+
+	public function testSelectLabel(): void {
+		$cities = ['BUD' => 'Budapest', 'KIN' => 'Kingston'];
+		$select = Select::create( 'city', $cities);
+		$select->activate('KIN');
+		$select->label('test', 'Test Select');
+		$this->assertSame(
+			'<label for="test">Test Select</label><select name="city" id="test"><option value="BUD">Budapest</option><option value="KIN" selected="selected">Kingston</option></select>',
+			(string) $select
+		);
+	}
+
+	public function testSelectMultiple(): void {
+		$cities = ['BUD' => 'Budapest', 'KIN' => 'Kingston'];
+		$select = Select::create( 'city', $cities);
+		$select->activate('KIN');
+		$select->multiple();
+		$this->assertSame(
+			'<select name="city" multiple="multiple"><option value="BUD">Budapest</option><option value="KIN" selected="selected">Kingston</option></select>',
+			(string) $select
+		);
+	}
+
+
 }

e618af6fcf14029a2644918210ae2a21532da995
Heiszler Norbert <heiszler.norbert@gmail.com>
2020-02-07 00:13:00 +0100

    Update ElementInterface.php
    Same here with the "magic methods". Furthermore the "parent methods" are part of this interface as well, don't need to write again.
    
    Set/Get methods should be where you use them, so "setTag", etc should be in MarkupInterface

--- src/Base/ElementInterface.php
+++ src/Base/ElementInterface.php
@@ -16,21 +16,7 @@ use Bpstr\Elements\Extension\ExtensionInterface;
  * @author bpstr <bpstr@gmx.tm>
  */
 interface ElementInterface extends MarkupInterface {
-
-	/**
-	 * Creates new instance of an HTML Element.
-	 *
-	 * @param string $tag
-	 *   HTML Tag of the element
-	 * @param mixed|null $content
-	 *   Content of the element
-	 * @param array $attributes
-	 *   Associative array with HTML attributes
-	 *
-	 * @return \Bpstr\Elements\Base\ElementInterface
-	 */
-	public static function create (string $tag, $content = NULL, iterable $attributes = []);
-
+	
 	/**
 	 * @param string $tag
 	 * @param string[] ...$classes
@@ -48,34 +34,6 @@ interface ElementInterface extends MarkupInterface {
 	 * @return bool
 	 */
 	public function is(string ...$tags): bool;
-
-
-	/**
-	 * @return string
-	 */
-	public function getTag(): string;
-
-	/**
-	 * Set tag of an element
-	 *
-	 * @param string $tag
-	 *
-	 * @return \Bpstr\Elements\Base\ElementInterface
-	 *
-	 * @see \Bpstr\Elements\Base\MarkupInterface::tag()
-	 */
-	public function setTag(string $tag): self;
-
-
-
-	/**
-	 * Returns a content entry with the given key or NULL.
-	 *
-	 * @param mixed|NULL $key
-	 *
-	 * @param mixed|NULL $default
-	 */
-	public function getContent($key = NULL, $default = NULL);
 
 	/**
 	 * Place content before other content.
@@ -117,48 +75,6 @@ interface ElementInterface extends MarkupInterface {
 	 * @return array
 	 */
 	public function getChildrenByTagname(string $tag): array;
-
-	/**
-	 * @param iterable $attributes
-	 *
-	 * @return \Bpstr\Elements\Base\ElementInterface
-	 */
-	public function attributes(iterable $attributes): self;
-
-	/**
-	 * @return \Bpstr\Elements\Base\ElementAttributeCollection
-	 */
-	public function getAttributes(): ElementAttributeCollection;
-
-	/**
-	 * @param string $name
-	 * @param null $default
-	 *
-	 * @return mixed
-	 */
-	public function getAttribute(string $name, $default = NULL);
-
-	/**
-	 * @param string $name
-	 * @param $value
-	 *
-	 * @return \Bpstr\Elements\Base\ElementInterface
-	 */
-	public function setAttribute(string $name, $value): ElementInterface;
-
-	/**
-	 * @param array $keys
-	 *
-	 * @return \Bpstr\Elements\Base\ElementInterface
-	 */
-	public function removeAttribute(... $keys): ElementInterface;
-
-	/**
-	 * @param string $name
-	 *
-	 * @return bool
-	 */
-	public function hasAttribute(string $name): bool;
 
 	/**
 	 * @param string $attr
@@ -239,16 +155,4 @@ interface ElementInterface extends MarkupInterface {
 	public function attachExtension(ExtensionInterface $extension);
 
 	public function detachExtension(string $name);
-
-	/**
-	 * Renders an HTML element as string.
-	 *
-	 * @return string
-	 */
-	public function render(): string;
-
-	public function __toString();
-
-	public function __invoke($key, $content);
-
 }
